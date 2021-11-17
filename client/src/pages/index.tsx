2eb49c660150a547aae976da942465b58f415738
Lorena <lorena_dasilvaelias@hotmail.com>
2021-11-17 11:53:34 -0300

    client finalizado

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,38 @@
-import type { NextPage } from 'next'
+import { useState, useEffect, useRef } from 'react'
+import gsap from 'gsap'
 
-const Home: NextPage = () => {
+import TemplateHome from 'templates/Home'
+import Overload from '../components/Overload'
+
+import ElementProvider from 'contexts/Index'
+
+export type ModalProps = {
+  status?: boolean
+}
+
+function Index() {
+  const statusRef = useRef(false)
+  const modalRef = useRef<HTMLDivElement>(null)
+  const [useModal, setModal] = useState<ModalProps>({})
+
+  useEffect(() => {
+    const { status } = useModal
+    if (statusRef.current && status === false) {
+      gsap.to(modalRef.current, {
+        duration: 0.5,
+        autoAlpha: 0
+      })
+    } else {
+      statusRef.current = true
+    }
+  }, [useModal])
+
   return (
-    <div>
-      <h1>API Mercado Pago</h1>
-    </div>
+    <ElementProvider {...{ useModal, setModal }}>
+      <TemplateHome />
+      <Overload {...{ useModal, modalRef }} />
+    </ElementProvider>
   )
 }
 
-export default Home
+export default Index
